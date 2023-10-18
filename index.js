d651110e45b9bcadd285ea8b31b80480857f3280
Afiq Razak <120654244+afiqrazak380@users.noreply.github.com>
2023-10-18 18:46:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// import {PI, getCircumference, getArea} from './math_util.js'
+
+// console.log(PI);
+
+// let circumference = getCircumference(3);
+// console.log(circumference);
+
+// let area = getArea(3);
+// console.log(area);
+
+
+import * as MathUtil from './math_util.js'
+
+console.log(MathUtil.PI);
+
+let circumference = MathUtil.getCircumference(3);
+console.log(circumference);
+
+let area = MathUtil.getArea(3);
+console.log(area);
